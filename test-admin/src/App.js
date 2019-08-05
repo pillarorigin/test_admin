166b51298a13d0cf60f06223da75ced112ebbe50
pillarorigin <youig6495@naver.com>
2019-08-05 11:41:40 +0900

    pillarorigin_2019.08.05

--- test-admin/src/App.js
+++ test-admin/src/App.js
@@ -1,8 +1,16 @@
-import React, { Component } from 'react';
-import {Admin} from 'react-admin';
+import React  from 'react';
+import {Admin, Resource, ListGuesser} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');\
-const App = () =><Admin dataProvider = {dataProvider} />;
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => (
+
+
+<Admin dataProvider = {dataProvider}>
+    <Resource name="users" list={ListGuesser} /> 
+    </Admin>
+)
+    
 
 export default App;
